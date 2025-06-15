171251361a60d484409d6b8a7492086a9afc88c9
davodhiambo <jesseomolo@yahoo.com>
2025-06-15 07:05:17 +0300

    stage progress

---
+++ src/components/login.jsx
@@ -0,0 +1,8 @@
+export function LoginForm() {
+    <div className="h-fit w-fit">
+        <div className="w-1/2 h-1/2 absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+            <div className="bg-[linear-gradient(#049730, #23f669)] w-12 h-12 absolute rounded-full left-[32%] top[10%]"></div>
+            <div className="bg-[linear-gradient(#ffd92f, #f09819)] w-12 h-12 absolute rounded-full right-[32%] bottom[10%]"></div>
+        </div>
+    </div>
+}
