88d5870761457137dfcfae34fd7675bbc3020c47
davodhiambo <jesseomolo@yahoo.com>
2025-06-15 06:33:00 +0300

    import tailwind in css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className="text-9xl text-white font-bold text-center bg-blue-500 h-screen">
+    <div className="text-9xl text-white font-bold flex justify-center align-middle bg-blue-500 h-screen w-screen">
       Hey
     </div>
   )
