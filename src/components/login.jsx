e9cff89726c38a01bdb076556e97178d6ed6aeff
davodhiambo <jesseomolo@yahoo.com>
2025-06-16 11:16:48 +0300

    style: redesign asthetics of login component.

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -2,13 +2,8 @@ import logoPath from '../assets/images/logo/logo.png';
 
 export function LoginForm() {
     return (
-    <div className="h-fit w-fit">
-        <div className="w-full h-full absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
-            <div className="green-circle w-35 h-35 absolute rounded-full left-[-8%] top-[6%] md:left-[20%] md:top-[22%] lg:left-[36%] lg:top-[12%] xl:left-[36%] xl:top-[12%]"></div>
-            <div className="orange-circle w-35 h-35 absolute rounded-full right-[-8%] bottom-[6%] md:right-[20%] md:bottom-[22%] xl:right-[36%] xl:bottom-[12%] 2xl:right-[36%] 2xl:bottom-[12%]"></div>
-        </div>
-
-        <form className='h-fit w-80 bg-white/15 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-[10px] backdrop-blur-md border border-white/10 shadow-[0_0_40px_rgba(8,7,16,0.6)] px-[35px] py-[50px] flex flex-col justify-center gap-6'>
+    <div className="min-h-screen h-fit w-screen relative">
+        <form className='h-fit w-80 bg-white/15 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-[10px] backdrop-blur-md border border-white/10 shadow-[0_0_40px_rgba(8,7,16,0.6)] px-[35px] py-[50px] flex flex-col justify-center gap-6 z-10'>
             <img src={logoPath} alt="Logo" className='h-30 w-auto max-w-full object-contain'></img>
                 <div className='flex flex-col justify-center'>
                     <label htmlFor="username" className='block text-white text-base font-medium'>Username</label>
@@ -20,6 +15,8 @@ export function LoginForm() {
                     <button className='mt-10 h-10 bg-white text-[#080710] p-0 text-lg font-semibold rounded-[5px] cursor-pointer'>Log In</button>
                 </div>
         </form>
+        <div className="green-circle w-24 h-24 absolute rounded-full left-[14%] top-[14%] md:left-[24%] md:top-[24%] lg:left-[31%] lg:top-[6%] xl:left-[34%] xl:top-[17%] 2xl:left-[38%] 2xl:top-[18%] overflow-hidden"></div>
+        <div className="orange-circle w-24 h-24 absolute rounded-full right-[14%] bottom-[14%] md:right-[24%] md:bottom-[24%] lg:right-[31%] lg:bottom-[6%] xl:right-[34%] xl:bottom-[17%] 2xl:right-[38%] 2xl:bottom-[18%] overflow-hidden"></div>
     </div>
     )
 }
