304d8a61b6363dad400993d0034a94c9c4b37e3b
davodhiambo <jesseomolo@yahoo.com>
2025-06-21 21:39:02 +0300

    routes: add route for dashboard component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { LoginForm } from './components/login'
+import { Dashboard } from './components/dashboard'
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginForm />} />
+      <Route path='/' element={<Dashboard />} />
     </Routes>
   )
 }
