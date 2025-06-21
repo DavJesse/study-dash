66c624c408a2fc4356ac73807eacb82b342d6c5b
davodhiambo <jesseomolo@yahoo.com>
2025-06-21 23:58:06 +0300

    feat: add fetchCurrentUser.

---
+++ src/lib/user.js
@@ -0,0 +1,27 @@
+import { API_URL } from "./auth";
+import { getToken } from "./auth";
+
+export async function fetchCurrentUser() {
+    const token = getToken();
+    if (!token) return null;
+
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+            query: `{
+                user {
+                    id
+                    login
+                    }
+                }`
+        }),
+    });
+
+    const { data, errors} = await response.json();
+    if (errors || !data.user) return null;
+    return data.user;
+}
