158333d3353fdde9b6048e4b2d37b3ae3e19abe9
davodhiambo <jesseomolo@yahoo.com>
2025-06-22 00:10:55 +0300

    feat: add useAuth.

--- src/lib/user.js
+++ src/lib/user.js
@@ -1,5 +1,6 @@
 import { API_URL } from "./auth";
 import { getToken } from "./auth";
+import { useState, useEffect } from "react";
 
 export async function fetchCurrentUser() {
     const token = getToken();
@@ -24,4 +25,27 @@ export async function fetchCurrentUser() {
     const { data, errors} = await response.json();
     if (errors || !data.user) return null;
     return data.user;
+}
+
+export function useAuth() {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const check = async () => {
+            if (!getToken()) {
+                setUser(null);
+                setLoading(false);
+                return;
+            }
+
+            const user = await fetchCurrentUser();
+            setUser(user);
+            setLoading(false);
+        }
+
+        check();
+    }, []);
+
+    return { user, loading, 'isLoggedIn': !!user};
 }
