b12c6cebb4436136f37ca50520fc6e3db0b17e4d
davodhiambo <jesseomolo@yahoo.com>
2025-06-24 11:58:53 +0300

    responsiveness: make name cardd responsive to medium screens.

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -23,19 +23,18 @@ console.log('Dashboard Data: ', data)
     return (
         <div className="flex flex-col">
             {/* Name Card */}
-            <div className="w-[80%] h-fit bg-white/15 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-[10px] backdrop-blur-md border border-white/10 shadow-[0_0_40px_rgba(8,7,16,0.6)] px-[35px] py-[30px] justify-center z-10">
+            <div className="w-[95%] xl:w-[80%] h-fit bg-white/15 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-[10px] backdrop-blur-md border border-white/10 shadow-[0_0_40px_rgba(8,7,16,0.6)] px-[20px] py-[20px] md:px-[35px] md:py-[30px] justify-center z-10">
                 <div className="flex flex-row justify-between items-center mb-6">
-                    <h1 className="text-white font-light">Welcome, <strong className="font-bold">{user[0].login}</strong></h1>
+                    <h1 className="text-white font-light text-[24px] md:text-[24px] xl:text-[51.3px]">Welcome, <strong className="font-bold">{user[0].login}</strong></h1>
                     <button
                         onClick={logout()}
-                        className="w-40 h-10 bg-white text-[#080710] p-0 text-lg font-semibold rounded-[5px] cursor-pointer"
+                        className="w-20 md:w-40 h-10 bg-white text-[#080710] p-0 text-lg font-semibold rounded-[5px] cursor-pointer"
                         >logout</button>
                 </div>
-
+                                        
                 {/* Stats Card */}
                 <div className="flex flex-row justify-between">
-                        <h2 className="text-gray-200 text-4xl font-light">Total XP: <strong className="font-bold text-[var(--green-accent-color)]">{totalXP}</strong></h2>
-                    
+                        <h2 className="text-gray-200 text-4xl font-light">Total XP: <strong className="font-bold text-[var(--green-accent-color)]">{totalXP}</strong></h2>                    
                         <h2 className="text-gray-200 text-4xl font-light">Pass/Fail Ratio: <strong className="font-bold text-[var(--orange-accent-color)]">{passCount}/{failCount}</strong></h2>
                 </div>
             </div>
