7201b3d381c2c74dee648392b8ed45a88d586539
davodhiambo <jesseomolo@yahoo.com>
2025-06-26 15:21:42 +0300

    style: display more information on name card.

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,6 +1,7 @@
 import { fetchDashboardData } from "../lib/dashboard"
 import { logout, getToken } from "../lib/auth"
 import { useState, useEffect} from "react"
+import { FaPhone, FaEnvelope } from "react-icons/fa"
 import { useNavigate } from "react-router-dom"
 import { XPLineGraph, PassFailPieChart } from "./graphs"
 
@@ -34,12 +35,31 @@ export function Dashboard() {
         logout()
         navigate('/login')
     }
-
+    
     // Data Extraction
     const {user, transaction, progress, result} = data;
-    console.log('transaction', transaction);
-    // Calculate Total XP
-    const totalXP = transaction.reduce((acc, tx) => acc + tx.amount, 0);
+    console.log('user', user);
+    // Calculate Total XP and Audit Ratio
+    let totalXP = transaction.reduce((acc, tx) => acc + tx.amount, 0);
+    const auditRatio = Number(user[0].auditRatio).toFixed(2);
+
+    // Bio Data extraction
+    const fullName = `${user[0].attrs.firstName} ${user[0].attrs.middleName} ${user[0].attrs.lastName}`;
+    const firstName = `${user[0].attrs.firstName}`;
+    const phone = `${user[0].attrs.phone}`;
+    const email = `${user[0].attrs.email}`;
+    const userName = `${user[0].login}`;
+    const gender = `${user[0].attrs.gender}`;
+    const country = `${user[0].attrs.country}`;
+    
+    // Format Total XP
+    if (totalXP > 1048576) {
+        totalXP = (totalXP / 1048576).toFixed(2) + 'MB';
+    } else if (totalXP > 1024) {
+        totalXP = (totalXP / 1024).toFixed(2) + 'KB';
+    } else {
+        totalXP = totalXP + 'B';
+    }
 
     // Calculate Pass/Fail Ratio
     const passCount = result.filter(r => r.grade === 1).length;
@@ -64,23 +84,74 @@ const sortedTransactions = [...transaction].sort(
     return (
         <div className="w-screen min-h-screen h-fit flex flex-col items-center justify-center gap-2">
             {/* Name Card */}
-            <div className="w-[95%] xl:w-[60%] h-fit bg-white/15 mt-1 rounded-[10px] backdrop-blur-md border border-white/10 shadow-[0_0_40px_rgba(8,7,16,0.6)] px-[10px] py-[10px] md:px-[35px] md:py-[30px]">
+            <div className="w-[95%] xl:w-[60%] h-fit bg-white/15 mt-1 rounded-[10px] backdrop-blur-md border border-white/10 shadow-[0_0_40px_rgba(8,7,16,0.6)] px-[10px] py-[10px] md:px-[35px] md:pt-[30px]">
+                {/* Welcome Statement */}
                 <div className="flex flex-row justify-between items-center">
-                    <h2 className="text-white font-light text-[22px] md:text-3xl xl:text-4xl">Welcome, <strong className="font-bold">{user[0].login}</strong></h2>
+                    <h2 className="text-white font-light text-[22px] md:text-3xl xl:text-4xl">Welcome, <strong className="font-bold">{firstName}</strong></h2>
+                  
                     <button
                         onClick={handleLogout}
                         className="w-20 md:w-40 h-10 bg-white text-[#080710] p-0 text-lg font-semibold rounded-[5px] cursor-pointer"
                         >logout</button>
-                </div>                                        
+                </div>
+
+                {/* Personal Information */}
+                <div className="flex flex-col items-center py-4">
+                    <div className="flex items-center justify-center gap-4 w-full">
+                      <hr className="w-full border-t border-gray-200" />
+                      <h2 className="text-gray-100 italic whitespace-nowrap">Personal Information</h2>
+                      <hr className="w-full border-t border-gray-200" />
+                    </div>
+                    {/* Bio Data */}
+                    <div className="flex flex-row justify-between w-full px-10">
+                      {/* Column 1 */}
+                      <div className="w-1/2 px-2">
+                        <h2 className="text-gray-100 font-light">
+                          Full Name: <strong>{fullName}</strong>
+                        </h2>
+                        <h2 className="text-gray-100 font-light">
+                          Username: <strong>{userName}</strong>
+                        </h2>
+                      </div>
+
+                      {/* Column 2 */}
+                      <div className="w-1/2 px-2">
+                        <h2 className="text-gray-100 font-light">
+                          Gender: <strong>{gender}</strong>
+                        </h2>
+                        <h2 className="text-gray-100 font-light">
+                          Campus Location: <strong>{country}</strong>
+                        </h2>
+                      </div>
+                    </div>
+
+                      {/* Contact Information */}
+                      <div className="flex flex-row gap-6 mt-4">
+                            <div>
+                                <FaPhone className="text-[var(--green-accent-color)] text-2xl" />
+                                <h2 className="text-gray-200">{phone}</h2>
+                            </div>
+                            <h2 className="text-gray-200">|</h2>
+                            <div>
+                              <FaEnvelope className="text-[var(--orange-accent-color)] text-2xl" />
+                              <h2 className="text-gray-200">{email}</h2>
+                            </div>
+                      </div>
+                  </div>                                        
             </div>
                 {/* Stats Card */}
                 <div className="flex flex-row w-[95%] xl:w-[60%] gap-2">
-                    <div className="w-[50%] h-fit bg-white/15 rounded-[10px] backdrop-blur-md border border-white/10 shadow-[0_0_40px_rgba(8,7,16,0.6)] px-[10px] py-[10px] md:px-[35px] md:py-[30px] flex flex-col z-10">
+                    <div className="w-[33.3%] h-fit bg-white/15 rounded-[10px] backdrop-blur-md border border-white/10 shadow-[0_0_40px_rgba(8,7,16,0.6)] px-[10px] py-[10px] md:px-[35px] md:py-[30px] flex flex-col z-10">
                         <h2 className="text-gray-200 md:text-2xl xl:text-[20px] font-light">Total XP:</h2>
                         <h2 className="font-bold text-[var(--green-accent-color)] text:2xl md:text-3xl"><strong>{totalXP}</strong></h2>         
                     </div>
 
-                    <div className="w-[50%] h-fit bg-white/15 rounded-[10px] backdrop-blur-md border border-white/10 shadow-[0_0_40px_rgba(8,7,16,0.6)] px-[10px] py-[10px] md:px-[35px] md:py-[30px] flex flex-col z-10">
+                    <div className="w-[33.3%] h-fit bg-white/15 rounded-[10px] backdrop-blur-md border border-white/10 shadow-[0_0_40px_rgba(8,7,16,0.6)] px-[10px] py-[10px] md:px-[35px] md:py-[30px] flex flex-col z-10">
+                        <h2 className="text-gray-200 md:text-2xl xl:text-[20px] font-light">Audit Ratio:</h2>
+                        <h2 className="font-bold text-[var(--green-accent-color)] text:2xl md:text-3xl"><strong>{auditRatio}</strong></h2>         
+                    </div>
+
+                    <div className="w-[33.3%] h-fit bg-white/15 rounded-[10px] backdrop-blur-md border border-white/10 shadow-[0_0_40px_rgba(8,7,16,0.6)] px-[10px] py-[10px] md:px-[35px] md:py-[30px] flex flex-col z-10">
                         <h2 className="text-gray-200 md:text-2xl xl:text-[20px] font-light">Pass/Fail Ratio:</h2>
                         <h2 className="font-bold text-[var(--orange-accent-color)] text:2xl md:text-3xl"><strong>{passCount}/{failCount}</strong></h2>
                     </div>
